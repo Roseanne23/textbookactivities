c73b4c2050fbe359a790c3cb755dd436b1e3c2ee
Roseanne23 <roseanne.gulosino@gmail.com>
2024-10-07 20:06:21 +0800

    Add: Post controller use callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'welcome#index'
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
 
-  get 'posts', to: 'posts#index'
-  post 'posts', to: 'posts#create'
-  get 'posts/new', to: 'posts#new'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id', to: 'posts#show', as: 'post'
-  patch 'posts/:id', to: 'posts#update'
-  put 'posts/:id', to: 'posts#update'
-  delete 'posts/:id', to: 'posts#destroy'
+  resources :posts
 end
