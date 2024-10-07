f1c011af1a951e3914ae34ef908b92e0935e2b87
Roseanne23 <roseanne.gulosino@gmail.com>
2024-10-07 17:50:47 +0800

    Add: Post show and edit page. Add destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
   end
@@ -8,8 +9,39 @@ class PostsController < ApplicationController
   end
 
   def create
+    # render json: params[:post]
+
+    # @post = Post.new
+    # @post.title = params[:post][:title]
+    # @post.content = params[:post][:content]
+
+    # @post = Post.new(title: params[:post][:title],
+    #                  content: params[:post][:content])
+
     @post = Post.new(params[:post].permit(:title, :content))
+
     if @post.save
+      redirect_to posts_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :content))
+      redirect_to posts_path
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
       redirect_to posts_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "welcome#index"
-
+  # root "articles#index"
+  root 'welcome#index'
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
+
   get 'posts', to: 'posts#index'
   post 'posts', to: 'posts#create'
   get 'posts/new', to: 'posts#new'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  patch 'posts/:id', to: 'posts#update'
+  put 'posts/:id', to: 'posts#update'
+  delete 'posts/:id', to: 'posts#destroy'
 end
