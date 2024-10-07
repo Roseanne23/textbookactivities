1c03f741beebb869ccd6265bffcd85f900a48e68
Roseanne23 <roseanne.gulosino@gmail.com>
2024-10-07 10:23:11 +0800

    Add: Post Page Creation

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
   get 'posts', to: 'posts#index'
+  post 'posts', to: 'posts#create'
+  get 'posts/new', to: 'posts#new'
 end
