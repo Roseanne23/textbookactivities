810265463f41a3be4ee9971469660f5c7b562907
Roseanne23 <roseanne.gulosino@gmail.com>
2024-10-11 00:03:51 +0800

    modified validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
   validates :title, presence: true
-  validates :title, length: { in: 4..100 }
-  validates :title, uniqueness: true
-  validates :content, presence: true, length: { minimum: 5, maximum: 4000 }
+  validates :content, presence: true
 end
